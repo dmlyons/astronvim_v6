0d26c295c805e1192d31c123ad2ddc385762374f
David Lyons <david@meshify.com>
2026-08-16 00:12:03 -0500

    Fix none-ls.nvim module name and apply golangci-lint config via extra_args

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,11 +4,11 @@
 return {
   "nvimtools/none-ls.nvim",
   opts = function(_, opts)
-    local null_ls = require "null_ls"
+    local null_ls = require "null-ls"
 
     opts.sources = require("astrocore").list_insert_unique(opts.sources, {
       null_ls.builtins.diagnostics.golangci_lint.with {
-        config_file = "/home/dl/src/carbon/.golangci.yaml",
+        extra_args = { "--config", "/home/dl/src/carbon/.golangci.yaml" },
       },
     })
   end,
